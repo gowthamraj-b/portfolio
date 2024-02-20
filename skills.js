bd72884e1d59d2a8a5a3f0c8cef3949a8a60b73c
gowthamrajlegend <gowthamrajlegend@gmail.com>
2024-02-20 18:02:09 +0530

    Updates

---
+++ skills.js
@@ -0,0 +1,17 @@
+const skills = [
+    {id: 1, title: 'HTML'},
+    {id: 2, title: 'CSS'},
+    {id: 3, title: 'JavaScript'},
+    {id: 4, title: 'React js'},
+    {id: 5, title: 'GIT'},
+    {id: 6, title: 'Github'},
+    {id: 7, title: 'Responsive Design'},
+    {id: 9, title: 'Python'},
+    {id: 10, title: 'Terminal'},
+    {id: 11, title: 'Linux'},
+    {id: 12, title: 'Flask'},
+    {id: 13, title: 'Mango DB Basics'},
+    {id: 14, title: 'Tailwind Css'},
+]
+
+export default skills;
